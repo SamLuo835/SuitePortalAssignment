c891a4815503a90e273a0a61a004c47c2e321e45
Sam Luo <sam-law@live.cn>
2024-08-13 22:51:08 -0400

    fix import path

--- apps/suite-portal/src/app/services/admin.service.ts
+++ apps/suite-portal/src/app/services/admin.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@suiteportal/api-interfaces';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { ADMIN_SERVICE_BASE_URL } from '../../configs/main-config';
+import { ADMIN_SERVICE_BASE_URL } from '../configs/main-config';
 
 @Injectable({
   providedIn: 'root',
